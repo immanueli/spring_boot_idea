e68427ebd3276084ac855c689c8c3ad4bca8f2b3
immanueli <lovehaolive@gmail.com>
2018-06-28 22:36:18 +0800

    1:测试

--- src/main/java/com/imooc/miaosha/MainApplication.java
+++ src/main/java/com/imooc/miaosha/MainApplication.java
@@ -3,6 +3,9 @@ package com.imooc.miaosha;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 主应用程序
+ */
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
