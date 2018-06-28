7c8422b84816d44c9ce1d4a2261b8be55e3532a0
immanueli <lovehaolive@gmail.com>
2018-06-28 14:00:06 +0800

    1:随便提交一下下

---
+++ src/main/java/com/imooc/miaosha/dao/DemoDao.java
@@ -0,0 +1,9 @@
+package com.imooc.miaosha.dao;
+
+/**
+ * @author zhangch
+ * @create 2018-06-28
+ * @desc
+ **/
+public class DemoDao {
+}
---
+++ src/main/java/com/imooc/miaosha/service/DemoService.java
@@ -0,0 +1,9 @@
+package com.imooc.miaosha.service;
+
+/**
+ * @author zhangch
+ * @create 2018-06-28
+ * @desc
+ **/
+public class DemoService {
+}
